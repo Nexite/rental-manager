47bd2313fb4ce21747192c637386111c72880706
Nexite <nikhil@gargmail.com>
2023-03-22 15:27:41 -0700

    begin layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,23 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import {
+  Box,
+  Center,
+  ChakraProvider,
+  extendTheme,
+  Heading,
+} from "@chakra-ui/react";
+import type { AppProps } from "next/app";
+
+const theme = extendTheme({
+  config: { initialColorMode: "light", useSystemColorMode: false },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Center p={"10"} bg="gray.500">
+        <Heading>Rental Manager</Heading>
+      </Center>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
